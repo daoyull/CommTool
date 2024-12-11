096861677603da31ec94081ce3346f70dde8092a
DaoYu <daoyu5313@163.com>
2024-12-11 22:51:21 +0800

    commit

--- NetTool.Module/Messages/SerialMessage.cs
+++ NetTool.Module/Messages/SerialMessage.cs
@@ -4,7 +4,14 @@ namespace NetTool.Module.Messages;
 
 public readonly struct SerialPortMessage(byte[] data) : IMessage
 {
+    /// <summary>
+    /// 接收时间
+    /// </summary>
     public DateTime Time { get; } = DateTime.Now;
+    
+    /// <summary>
+    /// 接收数据
+    /// </summary>
     public byte[] Data { get; } = data;
     
 }
